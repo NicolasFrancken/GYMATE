3f3d6ad88480a7428a3c52029a90ce5e6aeb33df
Nicolas Francken <franckennicolas@gmail.com>
2023-06-18 00:13:22 -0300

    Add files via upload

---
+++ src/components/ScrollToTop.js
@@ -0,0 +1,16 @@
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
+
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    document.documentElement.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "instant",
+    });
+  }, [pathname]);
+
+  return null;
+}
